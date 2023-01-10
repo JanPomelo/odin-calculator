d683e5b3c195ff6d516a866f509f36c824bf6997
Jan Steikowski <j.steikowski@aol.com>
2023-01-10 13:01:13 +0700

    made simple calc with 2 numbers work

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
+let displayCurrent = document.getElementById('currentNum');
+let displayPrevious = document.getElementById('prevResult');
 let buttonRows = [...document.getElementsByClassName('buttonRow')];
 console.log(buttonRows);
+
 let buttons = buttonRows.reduce((button,buttonRow) => {
     return [...button,
         ...buttonRow.querySelectorAll('div')]
 },[])
-for (button in buttons) {
-    buttons[button].addEventListener('click',hallo);
+
+let numButtons = buttons.reduce((numButs,button) => {
+    if(button.className === 'numBut') {
+        return [...numButs,
+        button];
+    } else {
+        return numButs;
+    }
+},[]); 
+
+let operatorButtons = buttons.reduce((opButs,button) => {
+    if(button.className === 'operatorBut') {
+        return [...opButs,
+        button];
+    } else {
+        return opButs;
+    }
+},[]); 
+
+for (button in numButtons) {
+    numButtons[button].addEventListener('click',function() {
+        writeNumToDisplay(this.querySelectorAll('p')[0].innerText);
+    });
 }
 
+for (button in operatorButtons) {
+    operatorButtons[button].addEventListener('click',function() {
+        writeOperatorToDisplay(this.querySelectorAll('p')[0].innerText);
+    })
+}
 
-function hallo() {
-    console.log('hallo');
+let resultButton = document.getElementById('resultBut');
+resultButton.addEventListener('click',function() {
+    doCalcs()
+});
+
+function writeNumToDisplay(number) {
+    displayCurrent.innerHTML += number;
+}
+
+function writeOperatorToDisplay(operator) {
+    displayCurrent.innerHTML += ' ' + operator + ' '; 
+}
+
+function doCalcs() {
+    let displayArray = displayCurrent.innerText.split(' ');
+    displayPrevious.innerText = displayCurrent.innerText;
+    displayCurrent.innerText = operate(displayArray[1],Number(displayArray[0]),Number(displayArray[2]));
 }
 
 function add(a,b) {
